47d0365dd35724a4db9156dc58403d843eabcaa4
crucified1215 <31074867+crucified1215@users.noreply.github.com>
2017-11-21 15:01:27 +0530

    Add files via upload

---
+++ lite_bench.java
@@ -0,0 +1,77 @@
+
+import java.util.*;
+
+public class lite_bench {
+	public static void main(String args[])
+	{
+		int a=1;
+		int agree;
+		java.util.Scanner s=new Scanner(System.in);
+		
+		/**END USER LICENCE AGREEMENT**/
+		System.out.println("     ***LITE BENCH***               (OPEN SOURCE)"
+				+ "\n\n1)simple trick : creates 95 Processes | 985 Threads \n"
+				+ " 25246 Handles | Max speed:0.69Ghz |  Utilaization: 27% |"
+				+ "\n2)Logical processing 4 | L3 cache: 2MB | L1 cache:224KB\n\nRecorded cpu usage:\n    *without:3%\n    *on run:69%"
+				+ "\n\n\n                             #AGREE & Continue=1\n"
+				+ "                             #EXIT=2"
+				+ "\n                             #Source code=3`");
+		
+		agree=s.nextInt();
+		int d=12;
+		long b=d*d*d;
+		long c=2;
+		if(agree==1)
+		{
+		while(a==1)
+		{
+			System.out.println(+c+"...counting");
+			c=b*b*b;
+			b++;d++;
+		}
+		}
+		if(agree==3){
+			{
+				System.out.println("import java.util.*;\r\n" + 
+						"\r\n" + 
+						"public class largest_number {\r\n" + 
+						"	public static void main(String args[])\r\n" + 
+						"	{\r\n" + 
+						"		int a=1;\r\n" + 
+						"		int agree;\r\n" + 
+						"		java.util.Scanner s=new Scanner(System.in);\r\n" + 
+						"		\r\n" + 
+						"		/**END USER LICENCE AGREEMENT**/\r\n" + 
+						"		System.out.println(\"1)Practically its hard to find\\n\"\r\n" + 
+						"				+ \" largest possible number using JAVA code\\n\"\r\n" + 
+						"				+ \"\\n2)This piece of code may benchmark your pc\"\r\n" + 
+						"				+ \"\\n\\n\\n                             AGREE=1\\n\"\r\n" + 
+						"				+ \"                             EXIT=2\"\r\n" + 
+						"				+ \"\\n                           Source code=3`\");\r\n" + 
+						"		\r\n" + 
+						"		agree=s.nextInt();\r\n" + 
+						"		int d=12;\r\n" + 
+						"		long b=d*d*d;\r\n" + 
+						"		long c=2;\r\n" + 
+						"		if(agree==1)\r\n" + 
+						"		{\r\n" + 
+						"		while(a==1)\r\n" + 
+						"		{\r\n" + 
+						"			System.out.println(+c+\"...counting\");\r\n" + 
+						"			c=b*b*b;\r\n" + 
+						"			b++;d++;\r\n" + 
+						"		}\r\n" + 
+						"		}\r\n" + 
+						"		if(agree==3){\r\n" + 
+						"			{\r\n" + 
+						"				System.out.println(\"\");\r\n" + 
+						"			}\r\n" + 
+						"		}\r\n" + 
+						"	}\r\n" + 
+						"\r\n" + 
+						"}");
+			}
+		}
+	}
+
+}
